0d06bf6df6389313ada9df19c898e4c6668358c0
Joshua Singer <josh.j.singer@gmail.com>
2014-10-09 23:28:08 -0500

    Comments

--- main.m
+++ main.m
@@ -12,16 +12,18 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
+    
+        // const are constants, the rest use types of float or double for casting because physics uses long decimals
         float const initPosition = 10;
         float position = 10;
         double deltaT = .1;
         double time = 0;
         float const initVelocity = 16;
-        float velocity = 16;
+        float velocity = 16; 
         float const acceleration = -9.8;
         
         
-        while (position > 0 ) {
+        while (position > 0 ) { // loop will run as long as that intial condition is ongoing
             velocity = velocity + (acceleration * deltaT);
             position = position + (velocity * deltaT);
             time = time + deltaT;
@@ -29,7 +31,7 @@ int main(int argc, const char * argv[]) {
         
         double halfTime = time / 2;
         
-        double max = .5 * acceleration * pow(halfTime, 2) + initVelocity * halfTime + initPosition;
+        double max = .5 * acceleration * pow(halfTime, 2) + initVelocity * halfTime + initPosition; //There's probably a cleaner way to do this, but it's late.
         
        
         printf("The ball will hit the ground at a time of %f, and the ball will reach a height of %f meters", time, max);
