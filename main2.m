2eeed62a3427876e7866fdca09858e1f8aee8476
Joshua Singer <josh.j.singer@gmail.com>
2014-10-10 12:21:43 -0500

    asdfa;sldkf

--- main2.m
+++ main2.m
@@ -31,7 +31,7 @@ int main(int argc, const char * argv[]) {
         
         
        
-        printf("The ball's position is  %f meters, and the ball's velocity is %f m/s", position, velocity);
+        printf("The ball's position is  %f meters, and the ball's velocity is %f m/s \n", position, velocity);
         
     
     
